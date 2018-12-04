85aafb09ff9f54c1299074c5ca13bc4885255a15
Ivan Nikitin <pierbezuhoff2016@gmail.com>
2018-12-05 02:35:18 +0300

    lab2-1 done

--- lab2/processes.c
+++ lab2/processes.c
@@ -18,6 +18,7 @@ int n_lines; // # lines in graph.txt
 sem_t *syncronization;
 int *count; // # of processes
 int *n_running;
+int *potential;
 
 /* Example:
 1 3
@@ -59,15 +60,18 @@ void spawn_process(int process_id) {
         fprintf(stderr, "strange process_id: %d\n", process_id);
     (*n_running)++;
     (*count)++;
-    printf("(%d/%d) +%d\n", *n_running, *count, process_id);
+    printf("(%d[%d]/%d) +%d\n", *n_running, *potential, *count, process_id);
     sem_post(syncronization);
-    spawn_children(process_id);
+    if (process_id < n_lines)
+        spawn_children(process_id);
     sem_wait(syncronization);
     (*n_running)--;
-    printf("(%d/%d) -%d\n", *n_running, *count, process_id);
-    if ((*n_running) == 0)
+    printf("(%d[%d]/%d) -%d\n", *n_running, *potential, *count, process_id);
+    (*potential)--;
+    if ((*potential) == 0)
         printf("%d processes spawned\n", *count);
     sem_post(syncronization);
+    exit(EXIT_SUCCESS);
 }
 
 void spawn_children(int process_id) {
@@ -76,17 +80,14 @@ void spawn_children(int process_id) {
     int j = 0;
     child_id = graph[process_id][j];
     while (child_id != -1) {
+        (*potential)++;
         if (fork() == 0) { // child
             sem_wait(syncronization);
             printf("%d -> %d\n", process_id, child_id);
             spawn_process(child_id);
-            exit(EXIT_SUCCESS);
         }
         j++;
-        if (j >= n_lines)
-            child_id = -1;
-        else
-            child_id = graph[process_id][j];
+        child_id = graph[process_id][j];
     }
 }
 
@@ -117,6 +118,8 @@ int main(int argc, char *argv[]) {
     sem_init(syncronization, 1, 1);
     count = shmat(shmget(IPC_PRIVATE, sizeof(int), IPC_CREAT | IPC_EXCL | 0666), NULL, 0);
     *count = 0;
+    potential = shmat(shmget(IPC_PRIVATE, sizeof(int), IPC_CREAT | IPC_EXCL | 0666), NULL, 0);
+    *potential = 1; // strange, but works
     n_running = shmat(shmget(IPC_PRIVATE, sizeof(int), IPC_CREAT | IPC_EXCL | 0666), NULL, 0);
     *n_running = 0;
     // start
